dea430928e0f38efd09ae21b5e47198e44fe7692
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2016-04-26 23:55:48 +0200

    Beginning of wiimote integration.

---
+++ Assets/WiimoteScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using WiimoteApi;
+
+public class WiimoteScript : MonoBehaviour {
+    private Wiimote wiimote;
+
+    void Start () {
+        WiimoteManager.FindWiimotes ();
+
+        if (WiimoteManager.HasWiimote ()) {
+            Debug.Log ("Wiimote is connected");
+            wiimote = WiimoteManager.Wiimotes [0];
+            wiimote.SendPlayerLED (true, false, false, true);
+
+            // activate wiimotionplus
+            if (wiimote.RequestIdentifyWiiMotionPlus ()) {
+                if (wiimote.ActivateWiiMotionPlus ()) {
+                    Debug.Log ("Wiimotion plus activated");
+                }
+            }
+        }
+    }
+
+    void Update () {
+    }
+
+    void OnApplicationQuit () {
+        WiimoteManager.Cleanup (wiimote);
+    }
+}
