9c3c0b557f1df45ba21e7ec524106b7ed4c17952
Pedro Caldeira <pedro.caldeira@bluewin.ch>
2016-05-03 23:30:47 +0200

    Refactor motion plus calibration

--- Assets/WiimoteScript.cs
+++ Assets/WiimoteScript.cs
@@ -4,112 +4,163 @@ using WiimoteApi;
 using System.Runtime.Remoting;
 using System.Threading;
 using System;
+using UnityEngine.Events;
+using System.Globalization;
 
-public class WiimoteScript : MonoBehaviour
-{
-	[Range (1, 150)]
-	public float coefficient = 1f;
-	public Vector3 MotionPlusOffset = new Vector3 (-2.0f, 2.2f, 14.2f);
+public class WiimoteScript : MonoBehaviour {
+    // minimal value to detect a motion
+    [Range (0.0001f, 1)]
+    public float detectionThreshold = 0.2f;
 
-	// minimal value to detect a motion
-	[Range (0.0001f, 1)]
-	public float detectionThreshold = 0.2f;
+    // experimental acceleration calibration data
+    private int[,] accelCalibrationData = {{ 529, 529, 617 },
+        { 522, 633, 512 },
+        { 630, 528, 513 }
+    };
 
-	// default rotation of the wiimote
-	private Quaternion defaultRotation;
+    // default rotation of the wiimote
+    private Quaternion defaultRotation;
 
-	private Wiimote wiimote;
+    private Wiimote wiimote;
 
-	private bool wmpActivated = false;
+    private bool wmpActivated = false;
 
-	void Start ()
-	{
-		// store default rotation quaternion
-		defaultRotation = transform.rotation;
+    void Start () {
+        // store default rotation quaternion
+        defaultRotation = transform.rotation;
 
-		WiimoteManager.FindWiimotes ();
+        WiimoteManager.FindWiimotes ();
 
-		if (WiimoteManager.HasWiimote ()) {
-			Debug.Log ("Wiimote is connected");
-			wiimote = WiimoteManager.Wiimotes [0];
-			wiimote.SendPlayerLED (true, false, false, false);
+        if (WiimoteManager.HasWiimote ()) {
+            Debug.Log ("Wiimote is connected");
+            wiimote = WiimoteManager.Wiimotes [0];
+            wiimote.SendPlayerLED (true, false, false, false);
 
-			// set output data format
-			wiimote.SendDataReportMode (InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
+            // set output data format
+            wiimote.SendDataReportMode (InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
 
-			wiimote.RequestIdentifyWiiMotionPlus ();
-		}
-	}
+            wiimote.RequestIdentifyWiiMotionPlus ();
 
-	void Update ()
-	{
-		if (wiimote != null) {
-			ActivateMotionPlus ();
-			ReadWiimoteEvents ();
-			ReadMotionPlus ();
-		}
-	}
+            wiimote.Accel.accel_calib = accelCalibrationData;
+        }
+    }
 
-	void OnApplicationQuit ()
-	{
-		if (wiimote != null) {
-			WiimoteManager.Cleanup (wiimote);
-		}
-	}
+    void Update () {
+        if (wiimote != null) {
+            ActivateMotionPlus ();
+            ReadWiimoteEvents ();
+            ReadMotionPlus ();
+        }
+    }
 
-	private void ActivateMotionPlus ()
-	{
-		if (!wmpActivated && wiimote.wmp_attached) {
-			wiimote.ActivateWiiMotionPlus ();
-			wmpActivated = true;
-			Debug.Log ("WiiMotionPlus activated");
-		}
-	}
+    void OnApplicationQuit () {
+        if (wiimote != null) {
+            WiimoteManager.Cleanup (wiimote);
+        }
+    }
 
-	private void ReadMotionPlus ()
-	{
-		if (wmpActivated && wiimote.current_ext == ExtensionController.MOTIONPLUS) {
-			MotionPlusData data = wiimote.MotionPlus;
-			Vector3 rawRotation = new Vector3 (data.PitchSpeed,
-				                      data.YawSpeed,
-				                      data.RollSpeed);
+    private void ActivateMotionPlus () {
+        if (!wmpActivated && wiimote.wmp_attached) {
+            wiimote.ActivateWiiMotionPlus ();
+            wmpActivated = true;
+            Debug.Log ("WiiMotionPlus activated");
+        }
+    }
 
-			Debug.Log ("raw rotation: " + rawRotation);
+    private IEnumerator CalibrateMotionPlus () {
+        if (wmpActivated && wiimote.current_ext == ExtensionController.MOTIONPLUS) {
+            Debug.Log ("Starting Motion Plus calibration");
+            Debug.Log ("Please place Motion Plus in a flat surface");
+            Debug.Log ("Waiting...");
+            yield return new WaitForSeconds (4);
+            Debug.Log ("Calibrating...");
+            MotionPlusData data = wiimote.MotionPlus;
+            data.SetZeroValues ();
+            Debug.Log ("MotionPlus was calibrated");
+        }
+    }
 
-			Vector3 offsetRotation = MotionPlusOffset + rawRotation;
-			Debug.Log ("offsetRotation :" + offsetRotation);
-			Vector3 rotationDegrees = offsetRotation / coefficient;
-			Debug.Log ("rotationdegrees :" + rotationDegrees);
-			Vector3 finalRotation = checkDeadZone (rotationDegrees, detectionThreshold);
-			Debug.Log ("finalRotation :" + finalRotation);
+    private void ReadMotionPlus () {
+        if (wmpActivated && wiimote.current_ext == ExtensionController.MOTIONPLUS) {
+            MotionPlusData data = wiimote.MotionPlus;
+            Vector3 rawRotation = new Vector3 (data.PitchSpeed,
+                                      data.YawSpeed,
+                                      data.RollSpeed);
 
-			transform.Rotate (-finalRotation, Space.Self);
-		}
-	}
+            //Debug.Log ("raw rotation: " + rawRotation);
 
-	private void ReadWiimoteEvents ()
-	{
-		int nbOfEvents;
-		do {
-			nbOfEvents = wiimote.ReadWiimoteData ();
-		} while (nbOfEvents > 0);
+            Vector3 offsetRotation = rawRotation;
+            //Debug.Log ("offsetRotation :" + offsetRotation);
+            Vector3 rotationDegrees = offsetRotation * Time.deltaTime;
+            //Debug.Log ("rotationdegrees :" + rotationDegrees);
+            Vector3 finalRotation = checkDeadZone (rotationDegrees, detectionThreshold);
+            //Debug.Log ("finalRotation :" + finalRotation);
 
-		if (wiimote.Button.a) {
-			Debug.Log ("Button a was pressed");
-			ResetWiimoteRotation ();
-		}
-	}
+            transform.Rotate (-finalRotation, Space.Self);
+        }
+    }
 
-	private void ResetWiimoteRotation ()
-	{
-		transform.rotation = defaultRotation;
-	}
+    public void StartAccelerometerCalibration () {
+        StartCoroutine (CalibrateAccel ());
+    }
 
-	private static Vector3 checkDeadZone (Vector3 input, float threshold)
-	{
-		float x = Mathf.Abs (input.x) < threshold ? 0 : input.x;
-		float y = Mathf.Abs (input.y) < threshold ? 0 : input.y;
-		float z = Mathf.Abs (input.z) < threshold ? 0 : input.z;
-		return new Vector3 (x, y, z);
-	}
+    public void StartMotionPlusCalibration () {
+        StartCoroutine (CalibrateMotionPlus ());
+    }
+
+    private IEnumerator CalibrateAccel () {
+        Debug.Log ("Starting calibration process..");
+        for (int i = 0; i < 3; i++) {
+            AccelCalibrationStep calibrationStep = (AccelCalibrationStep)i;
+            Debug.Log ("Please put wiimote in " + (AccelCalibrationStep)i + " position");
+            yield return new WaitForSeconds (3f);
+            Debug.Log ("Calibrating...");
+            wiimote.Accel.CalibrateAccel (calibrationStep);
+        }
+        Debug.Log ("Calibration done");
+        accelCalibrationData = wiimote.Accel.accel_calib;
+        PrintAccelCalibration ();
+    }
+
+    private Vector3 GetAccelVector () {
+        float accel_x;
+        float accel_y;
+        float accel_z;
+
+        float[] accel = wiimote.Accel.GetCalibratedAccelData ();
+        accel_x = accel [0];
+        accel_y = -accel [2];
+        accel_z = -accel [1];
+
+        return new Vector3 (accel_x, accel_y, accel_z).normalized;
+    }
+
+    private void ReadWiimoteEvents () {
+        int nbOfEvents;
+        do {
+            nbOfEvents = wiimote.ReadWiimoteData ();
+        } while (nbOfEvents > 0);
+
+        if (wiimote.Button.a) {
+            Debug.Log ("Button a was pressed");
+            ResetWiimoteRotation ();
+        }
+    }
+
+    private void ResetWiimoteRotation () {
+        transform.rotation = defaultRotation;
+    }
+
+    private static Vector3 checkDeadZone (Vector3 input, float threshold) {
+        float x = Mathf.Abs (input.x) < threshold ? 0 : input.x;
+        float y = Mathf.Abs (input.y) < threshold ? 0 : input.y;
+        float z = Mathf.Abs (input.z) < threshold ? 0 : input.z;
+        return new Vector3 (x, y, z);
+    }
+
+    private void PrintAccelCalibration () {
+        foreach (int i in accelCalibrationData) {
+            Debug.Log (i);
+        }
+    }
 }
